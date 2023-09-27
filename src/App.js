d69f5ac42e41903e75f9107a30a6dada70e931e6
Piyushpp02 <114925341+Piyushpp02@users.noreply.github.com>
2023-09-27 15:06:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import About from './About';
 import './App.css';
 import Navbar from './Navbar';
 import TextForm from './TextForm';
@@ -6,11 +7,11 @@ import TextForm from './TextForm';
 function App() {
   return (
    <>
-  
    <Navbar title="Titlebar" home="home2"/>
    <div className='container'>
-   {/* <Navbar/> */}
-   <TextForm heading="Enter the text to convert upper case"/>
+    <Navbar/> 
+   <TextForm heading="Enter the text to convert upper case"/> 
+  <About ></About> 
    </div>
    </>
   );
--- src/TextForm.js
+++ src/TextForm.js
@@ -6,31 +6,64 @@ export default function TextForm(props) {
     let newText=text.toUpperCase();
     setText(newText);
   };
+   const handleLowclick = () => {
+     console.log("lower was clicked");
+     let newText=text.toLowerCase();
+     setText(newText);
+   };
+   const handleclearclick = () => {
+    console.log("clear was clicked");
+   
+    setText('');
+  };
+
 
   const handleOnChange = (event) => {
     console.log("text was changed");
     setText(event.target.value);
   };
 
-  const [text, setText] = useState();
+  const [text, setText] = useState(" enter txt")
 //   text=varible
 //   setText=value of that varaible we cannt directly chnage text we need to change value of settext
   return (
-    <div>
+    <>
+    
+    <div className="container">
       <h1>{props.heading}</h1>
-      <div className="mb-3">
+      <div>
+        
         <textarea
+        
           className="form-control"
-          placeHolder="enter your text"
+         
+          
           value={text}
           onChange={handleOnChange}
           id="mybox"
           rows="8"
         ></textarea>
-      </div>
+        </div>
+
+      
       <button className="btn btn-primary" onClick={handleUpclick}>
         Click to uppercase
       </button>
+      <button className="btn btn-primary mx-5 my-2" onClick={handleLowclick}>
+        Click to lowercase
+      </button>
+      <button className="btn btn-primary mx-5 my-2"   onClick={handleclearclick}>
+        Click to clear 
+      </button>
+      </div>
+    <div className="container my-3" >
+    <h1>your text summary</h1>
+  
+     <p>{text.split(" ").length} no. of words and no of letters {text.length}</p> 
+     <p>{0.008*text.split(" ").length} Minutes to read</p>  
+    <h2>Preview</h2>
+    <p>{text}</p>
     </div>
-  );
+    </>
+  )
 }
